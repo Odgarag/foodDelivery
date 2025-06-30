dc309fc4f7daefe70bedfcb1be6190cfe88dce18
odgarag <odgarag.odko@gmail.com>
2025-06-30 11:50:50 +0800

    r

--- fe-food-delivery/src/app/page.tsx
+++ fe-food-delivery/src/app/page.tsx
@@ -17,26 +17,11 @@ export default function Home() {
   const [foods, setFoods] = useState<Record<string, FoodProps[]>>({})
 
   useEffect(() => {
-    axios
-      .get('https://fooddelivery-2r6v.onrender.com/foods')
-      .then((res) => {
-        const list: FoodProps[] = res.data.foods
-
-        const grouped: Record<string, FoodProps[]> = {}
-
-        list.forEach((item) => {
-          if (!grouped[item.category]) {
-            grouped[item.category] = []
-          }
-          grouped[item.category].push(item)
-        })
-
-        setFoods(grouped)
-      })
-      .catch((err) => {
-        console.error('Failed to fetch foods:', err)
-      })
-  }, [])
+    axios.get('https://fooddelivery-2r6v.onrender.com/foods').then((res) => {
+      const list: FoodProps[] = res.data.foods
+      setFoods
+    })
+  })
 
   return (
     <div>
